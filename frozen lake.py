7f3d5dd12f7760b0790811637c8767671c9868db
Luis Coronel <luiscoronelhidalgo@outlook.com>
2025-04-08 20:05:24 +0200

    Pruebas

--- frozen lake.py
+++ frozen lake.py
@@ -8,16 +8,33 @@ import random as rd
 def run():
     env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False, render_mode="human")
 
-    state = env.reset()[0]
-    terminated = False
-    truncated = False
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
 
-    while (not terminated and not truncated):
-        action =    env.action_space.sample()
-        new_state, reward, terminated, truncated, info = env.step(action)
-        state = new_state
-    
-    env.close()
+    alpha = 0.8
+    gamma = 0.95
+
+    epsilon = 1
+    epsilon_decay = 0.0001
+    epsilon_min = 0.01
+    rng = np.random.default_rng()
+
+    num_episodes = 10000
+    max_steps = 100
+
+    for episode in range(num_episodes):
+        state = env.reset()[0]
+        terminated = False
+        truncated = False
+
+        while (not terminated and not truncated):
+            action =    env.action_space.sample() # Actions: 0=left, 1=down, 2=right, 3=up
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            
+            Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[new_state]) - Q[state, action])  # Q-learning update rule
+
+            state = new_state
+        
+        env.close()
 
 if __name__ == "__main__":
     run()
