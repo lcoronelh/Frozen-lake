934650b6a3bcc7083d808aa8ad9ef8ecb7f69219
Luis Coronel <luiscoronelhidalgo@outlook.com>
2025-04-08 20:34:29 +0200

    Código terminado en archivo.py, pendiente de notebook

--- frozen lake.py
+++ frozen lake.py
@@ -66,4 +66,4 @@ def run(episodes, is_training=True, render=False):
 if __name__ == '__main__':
     # run(15000)
 
-    run(1000, is_training=True, render=True)
+    run(1, is_training=False, render=True)
